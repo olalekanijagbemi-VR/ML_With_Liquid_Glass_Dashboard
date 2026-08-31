0888468aab0cda12dc00789975d5dc415f9c3e34
iLake <ilake@iLakes-MacBook-Pro.local>
2026-08-31 18:10:33 +0100

    Remove debug mode for production

--- app.py
+++ app.py
@@ -95,4 +95,4 @@ def train():
     })
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
